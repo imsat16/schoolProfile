802461a0078317cf4c26fbdd4066d2e936d3ee85
ogi <ogirahmansyah96@gmail.com>
2023-06-21 12:13:38 +0700

    sampah

--- src/pages/article/index.tsx
+++ src/pages/article/index.tsx
@@ -1,9 +1,41 @@
+import { ArticleCard } from '@/components/Card'
 import Layout from '@/layouts'
+import axios from 'axios'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const ArticlePages = () => {
+
+  const [data,setData]:any = React.useState<any[]>([])
+  React.useEffect(()=>{
+    axios.get("/dummy.json")
+    .then(res=>{
+      setData(res.data)
+      console.log(res.data)
+    })
+  },[])
   return (
-    <Layout>ArticlePages</Layout>
+    <Layout>ArticlePages
+      <div>
+        <div className='text-red-400 box-border h-32 w-32 p-4 border-4 relative'>
+        <Image className="object-cover" src={"/banner.png"} alt='' fill/>
+        </div>
+        <div className='grid-cols-4 grid '>
+        {data.map((ctx:any,index:any)=>{
+         <div key={index}>
+          <ArticleCard/>
+         </div>
+          
+        })}
+        
+        </div>
+      </div>
+
+
+
+
+    </Layout>
   )
 }
 
--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,9 +1,24 @@
+import Input from '@/components/Input'
 import Layout from '@/layouts'
 import React from 'react'
 
 const Contact = () => {
   return (
-    <Layout>Contact</Layout>
+    <Layout>Contact
+      <div>
+        <div className='bg-#1E1E1E'>gambar
+        <p>ingin berbicara dengan kami Jangan ragu 
+           untuk menelepon, kirim email, Tweet kami
+           atau cukup lengkapi formulir pertanyaan.
+        </p>
+
+        </div>
+
+        <form action="">
+        <Input />
+        </form>
+      </div>
+    </Layout>
   )
 }
 
